db7b5badf8069929a9abd3b154a3ce278e7a6212
swapv24 <swapnilverlekar24@gmail.com>
2024-07-31 11:07:17 -0400

    added pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import Header from "./components/Header";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import Clients from "./components/Clients";
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const cache = new InMemoryCache({
   typePolicies:{
@@ -31,12 +33,17 @@ function App() {
   return (
     <>
     <ApolloProvider client={client}>
+      <Router>
     <Header/>
-    
+
     <div className="container">
-      <AddClientModal/>
-      <Clients/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/projects/:id' element={<Project />} />
+      <Route path='/*' element={<NotFound/>} />
+    </Routes>
     </div>
+    </Router>
     </ApolloProvider>
     </>
   );  
