8dd2755db0fd79fa32beb625b4806ae7fa9d249c
swapv24 <swapnilverlekar24@gmail.com>
2024-08-01 13:36:23 -0400

    code for edit project

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@apollo/client';
 import { GET_PROJECT } from '../queries/projectQuery';
 import ClientInfo from '../components/ClientInfo';
 import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
 
 export default function Project() {
   const { id } = useParams();
@@ -26,6 +27,7 @@ export default function Project() {
           <h5 className='mt-3'>Project Status</h5>
           <p className='lead'>{data.project.status}</p>
           <ClientInfo client={data.project.client}/>
+          <EditProjectForm project={data.project}/>
           <DeleteProjectButton projectId={data.project.id} />
 
         </div>
